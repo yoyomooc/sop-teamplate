34081b6f9000d596740233c7aafb99dde348e172
tommy ltm <werltm@hotmail.com>
2024-10-21 01:10:52 +0800

    Update build script to include run_started_at in environment variables

--- build/bot.ps1
+++ build/bot.ps1
@@ -13,7 +13,7 @@ Write-Host "$currentDate"+$currentDate
 
 $rootPath = Split-Path -Parent (Get-Location).Path
 
-# 读取当前项目配置
+# 读取当前项目配置 -- ci-config.json
 $ciConfigPath = Join-Path $rootPath "src" "ci-config.json"
 $ciConfig = (Get-Content -Path $ciConfigPath -Encoding UTF8) | ConvertFrom-Json
 
@@ -23,17 +23,18 @@ if ($ciConfig.mode -eq 'tag') {
 }
 $branchOrTag = $ciConfig.branch
 $commit = $ciConfig.commit
-$creationTime = [DateTime]::Parse($ciConfig.creationTime).ToUniversalTime().AddHours(8)
 
-Write-Host "$creationTime"+$creationTime
+Write-Host "$run_started_at"+$run_started_at
 
 $gitlabPipelineId = $ciConfig.gitlabPipelineId
 
 $workflowUrl = "https://github.com/${env:repository}/actions/runs/${env:run_id}"
 $pipelineUrl = "${env:GIT_REPO_PIPLINE}/${gitlabPipelineId}"
 
+#$BuildImageName
+
 # 计算构建时长
-$buildDuration = $currentDate - $creationTime
+$buildDuration = $currentDate - $run_started_at
 
 # 根据编译结果生成通知消息
 if ($BuildSuccess) {
@@ -57,7 +58,7 @@ $content = @"
                 "content": [
                     [{
                         "tag": "text",
-                        "text": "--- ${currentDateStr} ---\n${message}\n\n--- 构建信息 ---\n${branchOrTagKey}: ${branchOrTag}\n提交: ${commit}\n创建时间: ${creationTime}\n构建时长: ${buildDuration}\nGitHub 工作流: "
+                        "text": "当前时间： ${currentDateStr} ， ${message}\n\n--- 构建信息 ---\n${branchOrTagKey}: ${branchOrTag}\n提交: ${commit}\n创建时间: ${creationTime}\n构建时长: ${buildDuration}\nGitHub 工作流: "
                     }, {
                         "tag": "a",
                         "text": "查看详情",
