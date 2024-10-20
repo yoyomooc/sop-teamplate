172943e60dd345de97a56d66faa5a453d2680b36
tommy ltm <werltm@hotmail.com>
2024-10-21 02:00:47 +0800

    branchOrTagKey

--- build/bot.ps1
+++ build/bot.ps1
@@ -58,8 +58,38 @@ Write-Host "$成功的镜像名称："+$BuildImageName
 
 # 根据编译结果生成通知消息
 if ($BuildSuccess) {
-pwsh ./trigger_syncimage.ps1
-    $title = "sop-teamplate-CI编译成功通知"
+
+$uri = "https://code.52abp.com/api/v4/projects/337/trigger/pipeline"
+$ref = "sync-images"
+
+# 定义要传递的变量
+$ciJobMode = $ciConfig.mode  # 替换为实际的作业模式
+$branchOrTagKey = "main"     # 替换为实际的版本号
+
+
+
+$response = Invoke-RestMethod -Uri "https://code.52abp.com/api/v4/projects/337/trigger/pipeline" -Method Post -Form @{
+    token = $GITLAB_RUNNER_TOKEN
+    ref = "sync-images"
+    "variables[CI_JOB_MODE]" = $ciJobMode
+    "variables[Version]" = $branchOrTagKey
+}
+
+if ($response -eq $null) {
+    Write-Error "Pipeline trigger failed."
+} else {
+    Write-Output "Pipeline triggered successfully."
+}
+
+
+
+
+
+
+
+
+
+    $title = "sop-teamplate-CI编译成功通知 $ref $uri $ciJobMode $version"
     $message = "sop-teamplate-CI编译成功！✨ 镜像：$BuildImageName 构建成功！"
     $emoji = "🚀"
 } else {
